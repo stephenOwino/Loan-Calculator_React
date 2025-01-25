1b9639a5bf2d2f150f09b259837be19b93f8fdb2
stephenOwino <stephenowin233@gmail.com>
2025-01-25 23:58:13 +0300

    updated loanslice to catch erors only if operations to backend and to frontend suceedeed

--- src/slices/LoanSlice.js
+++ src/slices/LoanSlice.js
@@ -1,5 +1,6 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import LoanService from "../api/LoanService";
+import { toast } from "react-toastify";
 
 const initialState = {
 	loanData: {
@@ -17,9 +18,9 @@ const initialState = {
 // Apply for a loan
 export const applyForLoan = createAsyncThunk(
 	"loan/apply",
-	async (loanData, thunkAPI) => {
+	async ({ loanData, customerId }, thunkAPI) => {
 		try {
-			const response = await LoanService.applyForLoan(loanData);
+			const response = await LoanService.applyForLoan(loanData, customerId);
 			return response;
 		} catch (error) {
 			const message =
@@ -55,10 +56,14 @@ const loanSlice = createSlice({
 			.addCase(applyForLoan.fulfilled, (state, action) => {
 				state.loading = false;
 				state.appliedLoan = action.payload;
+				toast.success("Loan application submitted successfully!");
 			})
 			.addCase(applyForLoan.rejected, (state, action) => {
 				state.loading = false;
 				state.error = action.payload;
+				toast.error(
+					action.payload || "An error occurred while applying for the loan."
+				);
 			});
 	},
 });
